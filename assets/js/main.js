d01302f77277221ecd4e78d25fa429095d7fa9af
Lyoneel <41694-lyoneel@users.noreply.gitlab.com>
2023-01-09 06:03:18 -0300

    disable chroma css and remove logic for copy button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ jQuery(function($) {
 
     'use strict';
 
-    var _Blog = window._Blog || {};
+    var _LyCustom = window._LyCustom || {};
 
-    _Blog.externalUrl = function() {
+    _LyCustom.externalUrl = function() {
         $.expr[':'].external = function(obj) {
             return !obj.href.match(/^mailto\:/) &&
                 (obj.hostname != location.hostname);
@@ -14,7 +14,7 @@ jQuery(function($) {
 
     }
 
-    _Blog.changeTitle = function() {
+    _LyCustom.changeTitle = function() {
         var currentTitle = document.title;
         window.onblur = function() {
             document.title = currentTitle;
@@ -24,7 +24,7 @@ jQuery(function($) {
         }
     };
 
-    _Blog.toggleTheme = function() {
+    _LyCustom.toggleTheme = function() {
         const currentTheme = window.localStorage && window.localStorage.getItem('theme')
         const isDark = currentTheme === 'dark'
         $('body').toggleClass('dark-theme', isDark)
@@ -35,16 +35,88 @@ jQuery(function($) {
         })
     }
 
-    _Blog.toggleMobileMenu = function() {
+    _LyCustom.toggleMobileMenu = function() {
         $('.menu-toggle').on('click', () => {
             $('.menu-toggle').toggleClass('active')
             $('#mobile-menu').toggleClass('active')
         })
     }
 
+    /*
+    function createCopyButton(highlightDiv) {
+        const button = document.createElement("button");
+        button.className = "copy-code-button";
+        button.type = "button";
+        button.innerText = "Copy";
+        button.addEventListener("click", () => copyCodeToClipboard(button, highlightDiv));
+        addCopyButtonToDom(button, highlightDiv);
+    }
+
+    async function copyCodeToClipboard(button, highlightDiv) {
+        const codeToCopy = highlightDiv.querySelector(":last-child > .chroma > code").innerText;
+        try {
+            let result = await navigator.permissions.query({ name: "clipboard-write" });
+            if (result.state == "granted" || result.state == "prompt") {
+                await navigator.clipboard.writeText(codeToCopy);
+            } else {
+                copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+            }
+        } catch (_) {
+            copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+        }
+        finally {
+            codeWasCopied(button);
+        }
+    }
+
+    function copyCodeBlockExecCommand(codeToCopy, highlightDiv) {
+        const textArea = document.createElement("textArea");
+        textArea.contentEditable = 'true'
+        textArea.readOnly = 'false'
+        textArea.className = "copyable-text-area";
+        textArea.value = codeToCopy;
+        highlightDiv.insertBefore(textArea, highlightDiv.firstChild);
+        const range = document.createRange()
+        range.selectNodeContents(textArea)
+        const sel = window.getSelection()
+        sel.removeAllRanges()
+        sel.addRange(range)
+        textArea.setSelectionRange(0, 999999)
+        document.execCommand("copy");
+        highlightDiv.removeChild(textArea);
+    }
+
+    function codeWasCopied(button) {
+        button.blur();
+        button.innerText = "Copied!";
+        setTimeout(function() {
+            button.innerText = "Copy";
+        }, 2000);
+    }
+
+    function addCopyButtonToDom(button, highlightDiv) {
+        highlightDiv.insertBefore(button, highlightDiv.firstChild);
+        const wrapper = document.createElement("div");
+        wrapper.className = "highlight-wrapper";
+        highlightDiv.parentNode.insertBefore(wrapper, highlightDiv);
+        wrapper.appendChild(highlightDiv);
+    }
+
+    function copyButton(){
+        document.querySelectorAll(".highlight")
+            .forEach(highlightDiv => createCopyButton(highlightDiv));
+    }
+
+     */
+
+
+
+
+
     $(document).ready(function() {
-        _Blog.changeTitle()
-        _Blog.toggleTheme()
-        _Blog.toggleMobileMenu()
+        _LyCustom.changeTitle()
+        _LyCustom.toggleTheme()
+        _LyCustom.toggleMobileMenu()
+        copyButton()
     });
 });
